c86326bb1765399b2c187eb475d41a1099896aac
Jenny Huang <chenyuh565@gmail.com>
2023-03-18 23:10:37 +0800

    3rd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 // aos
 AOS.init();
 
-// UIkit
+
+UIkit.switcher('#floor').show(1);
+UIkit.switcher('#table').show(0);
+// $('#product-switch').click(function () {
+//     console.log("click")
+//     $('.container .uk-tab li:first-child').removeClass('uk-active')
+//     $('.container .uk-tab li:last-child').toggleClass('uk-active')
+//     $('.container .uk-switcher>li:first-child').removeClass('uk-active')
+//     $('.container .uk-switcher>li:last-child').toggleClass('uk-active')
+//   });
